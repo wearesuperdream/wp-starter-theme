9d88ffecb6be78943ebbf59bade9b9e48423c961
Sam Dean <damsean102@gmail.com>
2017-04-24 21:17:15 +0100

    Update Structure of Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,13 @@
-// Include gulp
-var gulp = require('gulp'); 
-
 // Set site URL to be called in tasks
 var siteUrl = 'http://superdream.dev/superdream-boilerplate';
+// Set site Path
+var sitePath = 'wp-content/themes/THEME_NAME_HERE/';
 
-// Include all required plugins
-var jshint = require('gulp-jshint'),
+// Include gulp and all required plugins
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
     sass = require('gulp-sass'),
     cssnano = require('gulp-cssnano'),
-    uncss = require('gulp-uncss');
     concat = require('gulp-concat'),
     rename = require('gulp-rename'),
     autoprefixer = require('gulp-autoprefixer'),
@@ -27,53 +26,50 @@ gulp.task('serve', ['sass'], function(){
 
 // Lint Task
 gulp.task('lint', function() {
-  return gulp.src('src/scripts/*.js')
+  return gulp.src(sitePath + 'src/scripts/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
 
 // Compile Our Sass
 gulp.task('sass', function() {
-  return gulp.src('src/sass/**/*.scss')
+  return gulp.src(sitePath + 'src/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
       browsers: ['last 2 versions', 'ie >= 9'],
       cascade: false
     }))
-    .pipe(gulp.dest('dist/css'))
-    .pipe(uncss({
-      html: [siteUrl, '*.html']
-    }))
+    .pipe(gulp.dest(sitePath + 'dist/css'))
     .pipe(rename('style.min.css'))
     .pipe(cssnano())
-    .pipe(gulp.dest('dist/css/min'))
+    .pipe(gulp.dest(sitePath + 'dist/css/min'))
     .pipe(browserSync.stream())
     .pipe(notify({ message: 'Styles task complete' }));
 });
 
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
-  return gulp.src('src/scripts/*.js')
+  return gulp.src(sitePath + 'src/scripts/*.js')
     .pipe(concat('scripts.js'))
-    .pipe(gulp.dest('dist/scripts'))
+    .pipe(gulp.dest(sitePath + 'dist/scripts'))
     .pipe(uglify())
     .pipe(rename('scripts.min.js'))
-    .pipe(gulp.dest('dist/scripts/min'))
+    .pipe(gulp.dest(sitePath + 'dist/scripts/min'))
     .pipe(browserSync.stream())
     .pipe(notify({ message: 'Script task complete' }));
 });
 
 //Optimise Images
 gulp.task('images', () =>
-  gulp.src('src/images/*')
+  gulp.src(sitePath + 'src/images/*')
       .pipe(imagemin())
-      .pipe(gulp.dest('dist/img'))
+      .pipe(gulp.dest(sitePath + 'dist/img'))
 );
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-  gulp.watch('src/sass/**/*.scss', ['sass']);
-  gulp.watch('src/scripts/**/*.js', ['lint', 'scripts']);
+  gulp.watch(sitePath + 'src/sass/**/*.scss', ['sass']);
+  gulp.watch(sitePath + 'src/scripts/**/*.js', ['lint', 'scripts']);
 });
 
 
